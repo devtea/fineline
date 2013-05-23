f6edf0ed1d53dac89a90fcb8a278e3112d7d5a18
celeborn <celeborn@localhost>
2013-05-22 21:12:39 -0500

    Adding ai_night

---
+++ ai_night.py
@@ -0,0 +1,41 @@
+"""
+ai_night.py - A Willie module to simulate some simple AI
+Copyright 2013, Tim Dreyer
+Licensed under the Eiffel Forum License 2.
+
+http://bitbucket.org/tdreyer/fineline
+"""
+import random
+
+def night(Willie, trigger):
+    """Responds to people saying good night"""
+
+    message = random.choice(("Goodnight","'Night","Later","Bye"))
+    punctuation = random.choice((".","","!"))
+
+    #Replies cause the bot to reply to the triggered message
+    Willie.reply(message + punctuation)
+
+prefix = r"($nickname:?,?\s+)?"
+meat = r"((good)?\s?'?(night|bye))|(later)"
+all = r"all|every\s?(body|one|pony|pone|poni)|mlpds"
+to_fineline = prefix + meat
+to_all = r".*" + meat + r"\s+" + all
+universal = r".*time (for me)?\s?(to|for)\s?(go to)\s?" + \
+        r"(bed|sleep)"
+
+night.rule = r"(" + to_fineline + r")|" + \
+        r"(" + to_all + r")|" + \
+        r"(" + universal + r")"
+
+#Priorities of 'high', 'medium', and 'low' work
+night.priority = 'high'
+
+#Willie is multithreaded by default.
+#night.thread = False
+
+#Limit in seconds of users ability to trigger module
+night.rate = 1000
+
+if __name__ == "__main__":
+    print __doc__.strip()
