3fecf4b9b9ea5f5d823f2007257bc1421fd7f705
Tim Dreyer <tdreyer1@gmail.com>
2013-05-26 22:41:14 -0500

    [ai_night] Formatting and improved matching

--- ai_night.py
+++ ai_night.py
@@ -16,23 +16,22 @@ def night(Willie, trigger):
     punctuation = random.choice((".","","!"))
     # Test statment to filter negetive statements
     Willie.debug("ai_night.py:night", trigger.bytes, "verbose")
-    #if ('not' and 'no' and 'at') not in trigger.bytes:
-    if (('not' not in trigger.bytes)  and ('no' not in trigger.bytes) and
-            ('at' not in trigger.bytes)):
+    # Use a set intersection to filter triggering lines by keyword
+    if not set(trigger.args[1].split()).intersection(set(['not','no','at'])):
         time.sleep(1)
         if random.uniform(0,1) > 0.5:
             Willie.reply(message + punctuation)
         else:
             Willie.say(message + " " + trigger.nick + punctuation)
 prefix = r"($nickname\:?,?\s+)"
-meat = r"((good|g)?\s?'?(night|bye)|(later))"
+meat = r"((good|g)?\s?'?(night|bye)|(later(s?)))"
 all = r"(all|(every\s?(body|one|pony|pone|poni))|mlpds|" + \
         "folks|guys|peoples?|$nickname)"
 to_fineline = prefix + meat
 to_all = r".*?" + meat + r",?\s+" + all
 universal = r".*?((time (for me)?\s?(to|for)\s?((go to)|(head))?\s?(to )?" + \
-        "(bed|sleep))|" + \
-        "(I'?m going to (go to)?\s?(bed|sleep|crash|(pass out))))"
+            "(bed|sleep))|" + \
+            "(I'?m ((((going to)|(gonna)) ((go)|(head off))?)|(heading off))\s?(to )?(bed|sleep|crash|(pass out))))"
 night.rule = r"(" + to_fineline + r")|" + \
         r"(" + to_all + r")|" + \
         r"(" + universal + r")"
