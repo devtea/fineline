761feac3c1ca0b2c0e187512bf795739948bd2a6
Tim Dreyer <tdreyer1@gmail.com>
2013-06-04 21:07:56 -0500

    [ai_isbot.py] Cleaned up formatting and messages.

--- ai_isbot.py
+++ ai_isbot.py
@@ -5,39 +5,29 @@ Licensed under the Eiffel Forum License 2.
 
 http://bitbucket.org/tdreyer/fineline
 """
-import random, time
+import random
+import time
 
 random.seed()
 
 def isbot(Willie, trigger):
     """Replies to queries about fineline being a bot"""
 
-    nomessage = random.choice(("Nope, I'm just fast",
-        "Nah, I just type fast and know a lot",
-        "What makes you think that?",
-        "Uh.....no?"))
-    afterthought = random.choice(("Oh, and I have hooves...",
-        "And I might have hooves...",
-        "See? I have hooves! *wiggles hooves*"))
-    punctuation = random.choice((".","","!"))
-
-    time.sleep(random.uniform(0,2))
-    Willie.say(nomessage + punctuation)
-
-    time.sleep(random.uniform(2,5))
-    Willie.say(afterthought)
-# Rules allow regex matches to PRIVMSG
+    time.sleep(random.uniform(1,2))
+    Willie.say(random.choice(("Nope, I'm just fast.",
+            "Nah, I just type really fast and know a lot.",
+            "What makes you think that?",
+            "lolno",
+            "Uh.....no?"
+            )))
+    time.sleep(random.uniform(3,5))
+    Willie.say(random.choice(("And I have hooves!",
+            "If I were a bot, how come I have hooves?",
+            "See? I have hooves! *wiggles hooves*"
+            )))
 isbot.rule = r'.*$nickname\:?,?\s+Are you a bot|.*$nickname (is )?a bot'
-
-# Priorities of 'high', 'medium', and 'low' work
 isbot.priority = 'medium'
-
-# Willie is multithreaded by default.
-#isbot.thread = False
-
-# Limit in seconds of users ability to trigger module
 isbot.rate = 300
-
 
 
 if __name__ == "__main__":
