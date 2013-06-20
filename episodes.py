f292ad68fa6649e3988e321185c6464daaf89586
Tim Dreyer <tdreyer1@gmail.com>
2013-06-19 20:19:02 -0500

    Modified episode loading to resolve issue 28. Started implementing episode management via IRC.

--- episodes.py
+++ episodes.py
@@ -11,58 +11,90 @@ import re
 random.seed()
 
 def setup(Willie):
-    #load data from database
+    reload(Willie)
+
+
+def reload(Willie):
     global episodes
-    episodes = []
+    episodes = {}
+    cnt = 0  # for debug
     for row in Willie.db.episodes.keys("season, episode"):
-        episodes.append(Willie.db.episodes.get(row,
-            ('season','episode','title'), ("season", "episode")))
-    Willie.debug("episodes.py", "Loaded " + str(len(episodes)) +
-            " episodes.", "verbose")
+        s, e, t = Willie.db.episodes.get(
+                row,
+                ('season','episode','title'),
+                ("season", "episode")
+                )
+        if s not in episodes:
+            episodes[s] = {}
+            Willie.debug('episodes.py', 'added season %i' % s, 'verbose')
+        if e not in episodes[s]:
+            episodes[s][e] = t
+            Willie.debug('episodes.py', 'added episode %i' % e, 'verbose')
+            cnt = cnt + 1  # for debug
+        else:
+            Willie.debug('episodes.py', 'Warning: Duplicate episode found!', 'warning')
 
-def say_ep(Willie, trigger, episode):
-    """Accepts a list optionally containing an int of the index of the ep"""
-    if len(episode) == 0:
-        Willie.debug("episodes.py:episode", "Episode not found",
-                "verbose")
-        Willie.reply("I can't seem to find that episode.")
-    else:
-        episode = episodes[episode[0]]
-        Willie.debug("episodes.py:episode", episode, "verbose")
+    Willie.debug("episodes.py", "Loaded %i eps." % cnt, "verbose")
 
-        sentence = ["The episode is Season", str(episode[0]) + ",",
-            "Episode", str(episode[1]) + ",", episode[2] + "." ]
-        Willie.reply(" ".join(sentence))
+
+def get_ep(se):
+    """Accepts a list containing season and episode"""
+    if len(se) == 2 and se[0] in episodes and se[1] in episodes[se[0]]:
+            title = episodes[se[0]][se[1]]
+            return "The episode is season %i, episode %i, %s." % (
+                se[0], se[1], title)
+    return "I can't seem to find that episode."
+
 
 def reload_eps(Willie, trigger):
     """ADMIN: Reloads cached episodes from the database."""
     Willie.debug("episodes.py:reload_eps", "Triggered", "verbose")
-    if trigger.owner:
+    if trigger.admin:
         Willie.say("Reloading episodes.")
-
-        #load data from database
-        global episodes
-        episodes = []
-        for row in Willie.db.episodes.keys("season, episode"):
-            episodes.append(Willie.db.episodes.get(row,
-                ('season','episode','title'), ("season", "episode")))
-        Willie.debug("episodes.py", "Loaded " + str(len(episodes)) +
-                " episodes.", "verbose")
-        Willie.say("Done. Loaded " + str(len(episodes)) + " episodes.")
-        Willie.debug("episodes.py", episodes, "verbose")
+        reload(Willie)
+        Willie.say("Done.")
     else:
         Willie.debug("episodes.py", trigger.nick +
                 " just tried to reload episodes...", "always")
 reload_eps.commands = ['reload_eps']
-reload_eps.priority = 'medium'
 reload_eps.example = ".reload_eps"
 
+
+# TODO
 def add_ep(Willie, trigger):
     """ADMIN: Adds an episode to the database. Admin only"""
     Willie.debug("episodes.py:add_ep", "Triggered", "verbose")
-    if trigger.owner:
-        Willie.debug("episodes.py", "This module is unfinished", "warning")
-        Willie.reply("Hey knucklehead, you never finished this module!")
+    if trigger.admin:
+        # assume input is SxxExx title~~~~~~
+        command = trigger.args[1].split()  # eg ['!test', 'S01E01', 'Title', ...]
+        if len(command) > 2:
+            #Test the second argument for sanity, eg 'S01E03'
+            if re.match(r'^S\d{1,2}E\d{1,2}$', command[1], flags=re.IGNORECASE):
+                Willie.debug("episodes.py:episode", "Ep is sane", "verbose")
+                season, __, ep = trigger.args[1].split()[1].upper().partition("E")
+                season = int(season.lstrip("S"))
+                ep = int(ep)
+                title = ' '.join(i for i in command if command.index(i) > 1)
+                Willie.debug('', 'Season %i, episode %i' % (season, ep), 'verbose')
+                message = get_ep([season, ep])
+                if message.startswith('T'):
+                    Willie.reply("That episode already exists!")
+                    Willie.reply(message)
+                else:
+                    Willie.db.episodes.update(
+                            [season, ep],  #values
+                            {'season': season, 'episode': ep, 'title': title},  #new vals
+                            ("season", "episode")  #Keys
+                            )
+                    reload(Willie)  # Ineffecient, but easy...
+                    Willie.reply("Successfully added!")
+            else:
+                Willie.debug("episodes.py:episode", "Argument is insane",
+                        "verbose")
+                Willie.reply("I don't understand that.")
+        else:
+            Willie.debug("episodes.py:episode", "Not enough args", "verbose")
+            randep(Willie, trigger)
     else:
         Willie.debug("episodes.py", trigger.nick +
                 " just tried to add an episode...", "always")
@@ -70,23 +102,19 @@ add_ep.commands = ['add_ep']
 add_ep.priority = 'medium'
 add_ep.example = ".add_ep"
 
+
 def episode(Willie, trigger):
     """Returns a specified episode by season and episode."""
     Willie.debug("episodes.py:episode", "Triggered", "verbose")
     #test the arguments returned, e.g. ['.episode', 'S01E03']
     if len(trigger.args[1].split()) == 2:
         #Test the second argument for sanity, eg 'S01E03'
-        if re.match(r'^S\d\dE\d\d$', trigger.args[1].split()[1],
+        if re.match(r'^S\d{1,2}E\d{1,2}$', trigger.args[1].split()[1],
                 flags=re.IGNORECASE):
             Willie.debug("episodes.py:episode", "Argument is sane",
                 "verbose")
             season, __, ep = trigger.args[1].split()[1].upper().partition("E")
-            index = (int(season.lstrip("S")), int(ep))
-            Willie.debug("episodes.py:episode", index, "verbose")
-            #get a table with the index number of the episode or null
-            index_no = [i for i, t in enumerate(episodes)
-                    if (t[0], t[1]) == index]
-            say_ep(Willie, trigger, index_no)
+            Willie.reply(get_ep([int(season.lstrip("S")), int(ep)]))
         else:
             Willie.debug("episodes.py:episode", "Argument is insane",
                     "verbose")
@@ -100,18 +128,21 @@ def episode(Willie, trigger):
         Willie.debug("episodes.py:episode", "Not enough args", "verbose")
         randep(Willie, trigger)
 episode.commands = ['episode', 'ep']
-episode.priority = 'medium'
 episode.rate = 35
 episode.example = ".episode S02E11"
+
 
 def randep(Willie, trigger):
     """Returns a random episode."""
     Willie.debug("episodes.py:randep", "Triggered", "verbose")
-    say_ep(Willie, trigger, [random.randint(0,len(episodes)-1)] )
+    season = random.randint(1,len(episodes))
+    episode = random.randint(1,len(episodes[season]))
+    Willie.reply(get_ep([season,episode]))
 randep.commands = ['randep', 'rep', 'randomep']
 randep.priority = 'medium'
 randep.rate = 35
 randep.example = ".randep"
+
 
 if __name__ == "__main__":
     print __doc__.strip()
