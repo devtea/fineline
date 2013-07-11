f67163e66f43a70793281ff61c2f8c9ba6dfa4b3
Tim Dreyer <tdreyer1@gmail.com>
2013-07-11 15:13:24 -0500

    Updated for 4.1.

--- rmlpds_checker.py
+++ rmlpds_checker.py
@@ -11,27 +11,29 @@ import time
 import random
 import re
 from datetime import datetime
-from urllib2 import HTTPError
+#from urllib2 import HTTPError
 
 import praw
 import praw.errors
-from praw.errors import InvalidUser, InvalidSubreddit
+from praw.errors import InvalidSubreddit
 from requests import HTTPError
 
 from colors import *
+from willie.module import interval
 
-_UA='FineLine IRC bot 0.1 by /u/tdreyer1'
-_check_interval = 3*60*60  # Seconds between checks
-_channels = ['#reddit-mlpds','#fineline_testing']  # Can be no more than 2 chans
+_UA = u'FineLine IRC bot 0.1 by /u/tdreyer1'
+_check_interval = 3 * 60 * 60  # Seconds between checks
+_channels = [u'#reddit-mlpds', u'#fineline_testing']
 
 # Use multiprocess handler for multiple bots/threads on same server
 praw_multi = praw.handlers.MultiprocessHandler()
 rc = praw.Reddit(user_agent=_UA, handler=praw_multi)
 
+
 def setup(willie):
     if "rmlpds_timer" not in willie.memory:
         # Set the timer and do the first check in a minute
-        willie.memory["rmlpds_timer"] = time.time()-_check_interval+60
+        willie.memory["rmlpds_timer"] = time.time() - _check_interval + 60
     if "rmlpds_timer_lock" not in willie.memory:
         willie.memory["rmlpds_timer_lock"] = threading.Lock()
 
@@ -39,28 +41,28 @@ def setup(willie):
 def filter_posts(posts):
     def is_livestream(post):
         livestreams = [
-                u'livestream.com',
-                u'twitch.tv',
-                u'justin.tv',
-                u'youtube.com',
-                u'ustream.tv',
-                u'nicovideo.jp'
-                ]
+            u'livestream.com',
+            u'twitch.tv',
+            u'justin.tv',
+            u'youtube.com',
+            u'ustream.tv',
+            u'nicovideo.jp'
+        ]
         for s in livestreams:
             if post.url and re.findall(s, post.url):
                 return True
-        if post.title and re.search('\[stream\]', post.title, re.IGNORECASE):
+        if post.title and re.search(u'\[stream\]', post.title, re.IGNORECASE):
             return True
         if post.is_self and post.selftext and re.search(
-                r'\b(live)?stream(ing)?\b',
+                ur'\b(live)?stream(ing)?\b',
                 post.title,
                 flags=re.IGNORECASE
-                ):
+        ):
             links = re.findall(
-                    r'https?://[^\[\]\(\)\{\}\<\>,!\s]+',
-                    post.selftext,
-                    flags=re.IGNORECASE
-                    )
+                ur'https?://[^\[\]\(\)\{\}\<\>,!\s]+',
+                post.selftext,
+                flags=re.IGNORECASE
+            )
             for link in links:
                 for s in livestreams:
                     if re.findall(s, link, flags=re.IGNORECASE):
@@ -68,89 +70,100 @@ def filter_posts(posts):
         return False
 
     def is_lounge(post):
-        if post.title and re.search(r'\blounge\b', post.title, re.IGNORECASE):
+        if post.title and re.search(
+            ur'\blounge\b',
+            post.title,
+            re.IGNORECASE
+        ):
             return True
         return False
 
     def is_theme(post):
-        if post.title and post.is_self and  re.match(
-                r'weekly (drawing )?theme',
-                post.title,
-                flags=re.IGNORECASE
-                ):
+        if post.title and post.is_self and re.match(
+            ur'weekly (drawing )?theme',
+            post.title,
+            flags=re.IGNORECASE
+        ):
             return True
         return False
 
     def is_biweekly(post):
         if post.title and post.is_self and re.search(
-                r'(st|rd|nd|th) bi-weekly( drawing)? challenge',
-                post.title,
-                flags=re.IGNORECASE
-                ):
+            ur'(st|rd|nd|th) bi-weekly( drawing)? challenge',
+            post.title,
+            flags=re.IGNORECASE
+        ):
             return True
         return False
 
     criticable = []
     if posts:
         for p in posts:
-            self = p.is_self  #Boolean
-            title = p.title
-            body = p.selftext
-            auth = p.author
-            url = p.url
-
-            if is_livestream(p) or is_lounge(p) or is_theme(p) or is_biweekly(p):
+            if is_livestream(p) or is_lounge(p) or is_theme(p) \
+                    or is_biweekly(p):
                 continue
             criticable.append(p)
     return criticable
 
 
+@interval(23)
 def rmlpds(willie):
     """Checks the subreddit for unattended recent posts."""
-    if willie.memory["rmlpds_timer"] > time.time()-_check_interval:
+    if willie.memory["rmlpds_timer"] > time.time() - _check_interval:
         return  # return if not enough time has elapsed since last full run
     willie.memory["rmlpds_timer_lock"].acquire()
     try:
         try:
-            mlpds = rc.get_subreddit('MLPDrawingSchool')
+            mlpds = rc.get_subreddit(u'MLPDrawingSchool')
         except (InvalidSubreddit, HTTPError):
             sub_exists = False
         else:
             sub_exists = True
         finally:
             # Set the timer for a 5 min. retry in case something goes wrong.
-            willie.memory["rmlpds_timer"] = time.time()-_check_interval+(5*60)
+            willie.memory["rmlpds_timer"] = time.time() - _check_interval + \
+                (5 * 60)
         if sub_exists:
-            willie.debug('timers_rmlpds.py', "Sub exists.", "verbose")
+            willie.debug(u'timers_rmlpds.py', u"Sub exists.", u"verbose")
             new_posts = mlpds.get_new(limit=50)
             uncommented = []
             for post in new_posts:
                 # No comments, and between 8 and 48 hrs old
                 if post.num_comments == 0 and \
-                        post.created_utc > (time.time()-(48*60*60)) and \
-                        post.created_utc < (time.time()-(8*60*60)):
-                    willie.debug('timers_rmlpds.py', "Adding post to list.", "verbose")
+                    post.created_utc > (time.time() - (48 * 60 * 60)) and \
+                        post.created_utc < (time.time() - (8 * 60 * 60)):
+                    willie.debug(
+                        u'timers_rmlpds.py',
+                        u"Adding post to list.",
+                        u"verbose"
+                    )
                     uncommented.append(post)
             uncommented = filter_posts(uncommented)
             if uncommented:
-                willie.debug('timers_rmlpds.py', "There are %i uncommented posts." % len(uncommented), "verbose")
+                willie.debug(
+                    u'timers_rmlpds.py',
+                    u"There are %i uncommented posts." % len(uncommented),
+                    u"verbose"
+                )
                 # There were posts, so set full timer
                 willie.memory["rmlpds_timer"] = time.time()
                 post = random.choice(uncommented)
                 c_date = datetime.utcfromtimestamp(post.created_utc)
                 f_date = c_date.strftime('%b %d')
-                num = 'is at least 1 post'
+                num = u'is at least 1 post'
                 if len(uncommented) > 1:
-                    num = 'are at least %i posts' % len(uncommented)
+                    num = u'are at least %i posts' % len(uncommented)
                 for chan in _channels:
-                    willie.msg(
+                    if chan in willie.channels:
+                        willie.msg(
                             chan,
-                            ("Hey everyone, there %s that might need " +
-                                "critique! Here's a random one: ") % num )
-                    nsfw = u''
-                    if post.over_18:
-                        nsfw =  u'[%s] ' % colorize(u'NSFW', ['red'], ['b'])
-                    willie.msg(
+                            (u"Hey everyone, there %s that might need " +
+                                u"critique! Here's a random one: ") % num
+                        )
+                        nsfw = u''
+                        if post.over_18:
+                            nsfw = u'[%s] ' % colorize(u'NSFW', ['red'], ['b'])
+                        willie.msg(
                             chan,
                             u'%s%s posted on %s – "%s" [ %s ] ' % (
                                 nsfw,
@@ -158,17 +171,19 @@ def rmlpds(willie):
                                 f_date,
                                 colorize(post.title, ['navy']),
                                 post.short_link
-                                ))
+                            )
+                        )
             else:
                 # There were no posts, so set a short timer
-                willie.memory["rmlpds_timer"] = time.time()-(_check_interval*3/4)
+                willie.memory["rmlpds_timer"] = time.time() - \
+                    (_check_interval * 3 / 4)
                 willie.debug(
-                        "timers_rmlpds",
-                        "No uncommented posts found.",
-                        "verbose"
-                        )
+                    u"timers_rmlpds",
+                    u"No uncommented posts found.",
+                    u"verbose"
+                )
         else:
-            willie.debug("timers_rmlpds", "Cannot check posts.", "warning")
+            willie.debug(u"timers_rmlpds", u"Cannot check posts.", u"warning")
     finally:
         willie.memory["rmlpds_timer_lock"].release()
 
