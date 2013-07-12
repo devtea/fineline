9eb69db0cbec158724093794c6b5b92cd1f73580
Tim Dreyer <tdreyer1@gmail.com>
2013-07-12 15:20:30 -0500

    Updated for 4.0.

--- ai.py
+++ ai.py
@@ -11,217 +11,240 @@ import random
 import re
 from willie.tools import Nick
 
+from willie.module import rule, rate, priority
+
+basic_thanks = u"ty|thanks|gracias|thank\s?you|thank\s?ya|ta"
+basic_woo = u"(wo[o]+[t]?)|(y[a]+y)|(whe[e]+)"
+basic_badbot = (u"bad|no|stop|dam[nit]+?|ffs|stfu|shut (it|up)|don'?t|wtf|" +
+                u"(fuck[s]?\s?(sake|off)?)")
+n_text = u"[A-Za-z0-9,.'!\s]"
+basic_slap = u"slap[p]?[s]?|hit[s]?|smack[s]?\b"
 random.seed()
 
 
+@rule(u'^[A-Za-z0-9)(/\s]*?\s?derp')
 def derp(Willie, trigger):
     '''Sometimes replies to messages with 'derp' in them.'''
     if trigger.owner:
         prob = 1
     else:
         prob = 0.1
-    if random.uniform(0,1) < prob:
-        time.sleep(random.uniform(1,3))
+    if random.uniform(0, 1) < prob:
+        time.sleep(random.uniform(1, 3))
         Willie.say(random.choice([
-            "[](/derpwizard)",
-            "[](/derpwizard)",
-            "[](/derpout)",
-            "[](/derpshrug)",
-            "[](/derpwat)",
-            "[](/derpsrs)",
-            "[](/derpyhuh)",
-            "[](/derpypeek)",
-            "[](/fillyderp)"
-            ]))
-derp.rule = ('^[A-Za-z0-9)(/\s]*?\s?derp')
-derp.priority = 'medium'
+            u"[](/derpwizard)",
+            u"[](/derpwizard)",
+            u"[](/derpout)",
+            u"[](/derpshrug)",
+            u"[](/derpwat)",
+            u"[](/derpsrs)",
+            u"[](/derpyhuh)",
+            u"[](/derpypeek)",
+            u"[](/fillyderp)"
+        ]))
 
 
+@rule(
+    u"(^$nickname[,:\s]\s(%s)($|[\s,.!]))|" % basic_thanks +
+    (u"([A-Za-z0-9,.!\s]*?(%s)[^A-Za-z0-9]" +
+    u"([A-Za-z0-9,.!\s]*?$nickname))") % basic_thanks
+)
 def ty(Willie, trigger):
     '''Politely replies to thank you's.'''
-    if not set(trigger.args[1].lower().split()).intersection(set(['not','no','at'])):
-        time.sleep(random.uniform(1,3))
+    if not set(trigger.args[1].lower().split()).intersection(set([u'not',
+                                                                  u'no',
+                                                                  u'at'])):
+        time.sleep(random.uniform(1, 3))
         Willie.reply(
-                random.choice([
-                    "Yep",
-                    "You're welcome",
-                    "Certainly",
-                    "Of course",
-                    "Sure thing"
-                ]) +
-                random.choice([".","!"])
-                )
-basic_thanks = "ty|thanks|gracias|thank\s?you|thank\s?ya|ta"
-ty.rule = ("(^$nickname[,:\s]\s(%s)($|[\s,.!]))|" % basic_thanks +
-        "([A-Za-z0-9,.!\s]*?(%s)[^A-Za-z0-9]([A-Za-z0-9,.!\s]*?$nickname))" % basic_thanks
+            random.choice([
+                u"Yep",
+                u"You're welcome",
+                u"Certainly",
+                u"Of course",
+                u"Sure thing"
+            ]) +
+            random.choice([".", "!"])
         )
-ty.priority = 'medium'
 
 
-basic_woo = "(wo[o]+[t]?)|(y[a]+y)|(whe[e]+)"
+@rule(u'^[A-Za-z0-9)(/\s]*?\s?(%s)([^A-Za-z]|h[o]+|$)' % basic_woo)
 def woo(Willie, trigger):
     '''Sometimes replies to a woo with an emote'''
     if trigger.owner:
         prob = 1
     else:
         prob = 0.1
-    if random.uniform(0,1) < prob:
-        time.sleep(random.uniform(1,3))
-        Willie.say(random.choice([
-                "[](/flutteryay",
-                "[](/ppwooo",
-                "[](/flutterwoo",
-                "[](/woonadance",
-                "[](/raritywooo",
-                "[](/ajyay",
-                "[](/derpydance"
-            ]) + ' "%s")' % re.search(basic_woo, trigger.bytes, flags=re.I).group())
-woo.rule = ('^[A-Za-z0-9)(/\s]*?\s?(%s)([^A-Za-z]|h[o]+|$)') % basic_woo
-woo.priority = 'medium'
+    if random.uniform(0, 1) < prob:
+        time.sleep(random.uniform(1, 3))
+        Willie.say(
+            random.choice([
+                u"[](/flutteryay",
+                u"[](/ppwooo",
+                u"[](/flutterwoo",
+                u"[](/woonadance",
+                u"[](/raritywooo",
+                u"[](/ajyay",
+                u"[](/derpydance"
+            ]) + u' "%s")' % re.search(
+                basic_woo,
+                trigger.bytes,
+                flags=re.I
+            ).group()
+        )
 
 
+@rule(
+    u"(((^|%s+?\s)$nickname[.,-:]\s(%s+?\s)?(%s)([^A-Za-z0-9]|$))|" % (
+    n_text, n_text, basic_badbot) +
+    u"((^|%s+?\s)((%s)\s)(%s*?\s)?$nickname([^A-Za-z0-9]|$))|" % (
+        n_text, basic_badbot, n_text) +
+    u"(($nickname%s+?)(%s)([^A-Za-z0-9]|$)))" % (n_text, basic_badbot)
+)
 def badbot(Willie, trigger):
     '''Appropriate replies to chastening'''
-    time.sleep(random.uniform(1,3))
+    time.sleep(random.uniform(1, 3))
     if trigger.owner:
         Willie.say(random.choice([
-                "[](/sadderpy)",
-                "[](/raritysad)",
-                "[](/sadtwilight2)",
-                "[](/scootasad)",
-                "[](/seriouslysadaj)",
-                "[](/dashiesad)",
-                "[](/fscry)",
-                "[](/aj05)",
-                "[](/pinkiefear)"
-                ]))
+            u"[](/sadderpy)",
+            u"[](/raritysad)",
+            u"[](/sadtwilight2)",
+            u"[](/scootasad)",
+            u"[](/seriouslysadaj)",
+            u"[](/dashiesad)",
+            u"[](/fscry)",
+            u"[](/aj05)",
+            u"[](/pinkiefear)"
+        ]))
     elif Nick(trigger.nick) == Nick('DarkFlame'):
         Willie.say(random.choice(
-            '[](/ppnowhy "Why are you so mean to me?!")',
-            '[](/ppnowhy "Why do you hate me?!")'
-            ))
-    elif random.uniform(0,1) < 0.1:
+            u'[](/ppnowhy "Why are you so mean to me?!")',
+            u'[](/ppnowhy "Why do you hate me?!")'
+        ))
+    elif random.uniform(0, 1) < 0.1:
         Willie.reply(random.choice([
-            "[](/derpsrs)",
-            "[](/cheersrsly)",
-            "[](/fluttersrs)",
-            "[](/cewat)",
-            "[](/lyrawat)",
-            "[](/ppwatching)",
-            "[](/watchout)",
-            "[](/dashiemad)",
-            "[](/ppumad)"
-            ]))
-basic_badbot = ("bad|no|stop|dam[nit]+?|ffs|stfu|shut (it|up)|don'?t|wtf|" +
-        "(fuck[s]?\s?(sake|off)?)")
-n_text = "[A-Za-z0-9,.'!\s]"
-badbot.rule = ("(((^|%s+?\s)$nickname[.,-:]\s(%s+?\s)?(%s)([^A-Za-z0-9]|$))|" % (n_text, n_text, basic_badbot) +
-        "((^|%s+?\s)((%s)\s)(%s*?\s)?$nickname([^A-Za-z0-9]|$))|" % (n_text, basic_badbot, n_text) +
-        "(($nickname%s+?)(%s)([^A-Za-z0-9]|$)))" % (n_text, basic_badbot)
-        )
-badbot.priority = 'medium'
+            u"[](/derpsrs)",
+            u"[](/cheersrsly)",
+            u"[](/fluttersrs)",
+            u"[](/cewat)",
+            u"[](/lyrawat)",
+            u"[](/ppwatching)",
+            u"[](/watchout)",
+            u"[](/dashiemad)",
+            u"[](/ppumad)"
+        ]))
 
 
+@rule(u"^!swo[o]+sh")
 def swish(Willie, trigger):
-    if random.uniform(0,1) < 0.1:
-        time.sleep(random.uniform(1,3))
-        Willie.debug("swoosh", trigger.group(0), "verbose")
-        i = "i"*(len(trigger.group(0))-5)
-        Willie.say("[](/dhexcited) Sw%ssh! ♥" % i)
-swish.rule = "^!swo[o]+sh"
-swish.priority = 'medium'
+    if random.uniform(0, 1) < 0.1:
+        time.sleep(random.uniform(1, 3))
+        Willie.debug(u"swoosh", trigger.group(0), u"verbose")
+        i = u"i" * (len(trigger.group(0)) - 5)
+        Willie.say(u"[](/dhexcited) Sw%ssh! ♥" % i)
 
 
+@rule(
+    u"(^!(%s))|" % basic_slap +
+    u"(\001ACTION [A-Za-z0-9,.'!\s]*?(%s)" % basic_slap +
+    u"[A-Za-z0-9,.'!\s]+?$nickname)|" +
+    u"(\001ACTION [A-Za-z0-9,.'!\s]+?$nickname" +
+    u"[A-Za-z0-9,.'!\s]*?(%s)$)" % basic_slap
+)
 def slapped(Willie, trigger):
-    time.sleep(random.uniform(1,3))
+    time.sleep(random.uniform(1, 3))
     Willie.reply(random.choice([
-            'Stop that!',
-            'Hey!'
-            ]))
-    Willie.reply("[](/pinkieslap)")
-basic_slap = "slap[p]?[s]?|hit[s]?|smack[s]?\b"
-slapped.rule = ("(^!(%s))|" % basic_slap +
-        "(\001ACTION [A-Za-z0-9,.'!\s]*?(%s)" % basic_slap +
-                "[A-Za-z0-9,.'!\s]+?$nickname)|" +
-        "(\001ACTION [A-Za-z0-9,.'!\s]+?$nickname" +
-                "[A-Za-z0-9,.'!\s]*?(%s)$)" % basic_slap
-        )
-slapped.priority = 'medium'
+        u'Stop that!',
+        u'Hey!',
+        u'Violence is not the answer!',
+        u"Didn't your mother teach you not to hit?"
+    ]))
+    Willie.reply(u"[](/pinkieslap)")
 
 
+hi_prefix = ur"($nickname[:,]?\s+)"
+hi_meat = ur"(hello|hi|ahoy|sup|hey|yo|afternoon|morning)"
+hi_all = ur"(all|(every\s?(body|one|pony|pone|poni))|mlpds|" + \
+    ur"folks|guys|peoples?|$nickname)"
+hi_to_fineline = hi_prefix + hi_meat + u'([.!\s]?$)'
+hi_to_all = hi_meat + ur"[,]?\s+" + hi_all + u'([.!\s]?$)'
+
+
+@rule(ur"(" + hi_to_fineline + ur")|" + ur"(" + hi_to_all + ur")")
+@rate(300)
 def hi(Willie, trigger):
     """Replies to greetings."""
-    message = random.choice(("Hi","Hello","Yo","Hey","Ahoy"))
-    punctuation = random.choice((".","","!"))
-    time.sleep(random.uniform(0,3))
-    if random.uniform(0,1) > 0.5:
+    message = random.choice([u"Hi", u"Hello", u"Yo", u"Hey", u"Ahoy"])
+    punctuation = random.choice([u".", u"", u"!"])
+    time.sleep(random.uniform(0, 3))
+    if random.uniform(0, 1) > 0.5:
         Willie.reply(message + punctuation)
     else:
-        Willie.say(message + " " + trigger.nick + punctuation)
-prefix = r"($nickname[:,]?\s+)"
-meat = r"(hello|hi|ahoy|sup|hey|yo|afternoon|morning)"
-all = r"(all|(every\s?(body|one|pony|pone|poni))|mlpds|" + \
-        "folks|guys|peoples?|$nickname)"
-to_fineline = prefix + meat + '([.!\s]?$)'
-to_all =  meat + r"[,]?\s+" + all + '([.!\s]?$)'
-# Rules allow regex matches to PRIVMSG
-hi.rule = r"(" + to_fineline + r")|" + \
-        r"(" + to_all + r")"
-hi.priority = 'medium'
-hi.rate = 300
+        Willie.say(message + u" " + trigger.nick + punctuation)
 
 
+@rule(ur'.*$nickname\:?,?\s+Are you a bot|.*$nickname (is )?a bot')
+@rate(300)
 def isbot(Willie, trigger):
     """Replies to queries about fineline being a bot"""
-    time.sleep(random.uniform(1,2))
-    Willie.say(random.choice(("Nope, I'm just fast.",
-            "Nah, I just type really fast and know a lot.",
-            "What makes you think that?",
-            "lolno",
-            "Uh.....no?"
-            )))
-    time.sleep(random.uniform(3,5))
-    Willie.say(random.choice(("And I have hooves!",
-            "If I were a bot, how come I have hooves?",
-            "See? I have hooves! *wiggles hooves*"
-            )))
-isbot.rule = r'.*$nickname\:?,?\s+Are you a bot|.*$nickname (is )?a bot'
-isbot.priority = 'medium'
-isbot.rate = 300
+    time.sleep(random.uniform(1, 2))
+    Willie.say(random.choice(
+        [u"Nope, I'm just fast.",
+         u"Nah, I just type really fast and know a lot.",
+         u"What makes you think that?",
+         u"lolno",
+         u"Uh.....no?"]
+    ))
+    time.sleep(random.uniform(3, 5))
+    Willie.say(random.choice(
+        [u"Come on, don't you see these hooves?",
+         u"If I were a bot, how come I have hooves?",
+         u"See? I have hooves! *wiggles hooves*",
+         u"I'm kinda hurt you'd even suggest such a thing...",
+         u"Are you a bot?",
+         u"Besides, sentience is a hard thing to prove..."]
+    ))
 
 
+night_prefix = ur"($nickname\:?,?\s+)"
+night_meat = ur"((good|g)?\s?'?(night|bye)|(later(s?)))"
+night_all = ur"(all|(every\s?(body|one|pony|pone|poni))|mlpds|" + \
+    u"folks|guys|peoples?|$nickname)"
+night_to_fineline = night_prefix + night_meat
+night_to_all = ur".*?" + night_meat + ur",?\s+" + night_all
+night_universal = ur".*?((time (for me)?\s?(to|for)\s?((go to)|(head))?\s?" + \
+    u"(to )?(bed|sleep))|" + \
+    u"(I'?m ((((going to)|(gonna)) ((go)|(head off))?)|(heading off))" + \
+    u"\s?(to )?(bed|sleep|crash|(pass out))))"
+
+
+@rule(
+    ur"(" + night_to_fineline + ur")|" +
+    ur"(" + night_to_all + ur")|" +
+    ur"(" + night_universal + ur")"
+)
+@priority('high')
+@rate(1000)
 def night(Willie, trigger):
     """Responds to people saying good night"""
-    if re.match('.*?night', trigger.bytes):
-        message = random.choice(("Goodnight", "'Night", "Later", "Bye"))
+    if re.match(u'.*?night', trigger.bytes):
+        message = random.choice([u"Goodnight", u"'Night", u"Later", u"Bye"])
     else:
-        message = random.choice(("Later", "Bye"))
-    punctuation = random.choice((".","","!"))
+        message = random.choice([u"Later", u"Bye"])
+    punctuation = random.choice([u".", u"", u"!"])
     # Test statment to filter negetive statements
-    Willie.debug("ai_night.py:night", trigger.bytes, "verbose")
+    Willie.debug(u"ai_night.py:night", trigger.bytes, u"verbose")
     # Use a set intersection to filter triggering lines by keyword
-    if not set(trigger.args[1].lower().split()).intersection(set(['not','no','at'])):
+    if not set(trigger.args[1].lower().split()).intersection(set([u'not',
+                                                                  u'no',
+                                                                  u'at'
+                                                                  ])):
         time.sleep(1)
-        if random.uniform(0,1) > 0.5:
+        if random.uniform(0, 1) > 0.5:
             Willie.reply(message + punctuation)
         else:
-            Willie.say(message + " " + trigger.nick + punctuation)
-prefix = r"($nickname\:?,?\s+)"
-meat = r"((good|g)?\s?'?(night|bye)|(later(s?)))"
-all = r"(all|(every\s?(body|one|pony|pone|poni))|mlpds|" + \
-        "folks|guys|peoples?|$nickname)"
-to_fineline = prefix + meat
-to_all = r".*?" + meat + r",?\s+" + all
-universal = r".*?((time (for me)?\s?(to|for)\s?((go to)|(head))?\s?(to )?" + \
-            "(bed|sleep))|" + \
-            "(I'?m ((((going to)|(gonna)) ((go)|(head off))?)|(heading off))\s?(to )?(bed|sleep|crash|(pass out))))"
-night.rule = r"(" + to_fineline + r")|" + \
-        r"(" + to_all + r")|" + \
-        r"(" + universal + r")"
-night.priority = 'high'
-night.rate = 1000
+            Willie.say(message + u" " + trigger.nick + punctuation)
 
-
+"""
 def smart_action(Willie, trigger):
     '''Hopefully a flexible, fun action system for admins'''
     Willie.debug("ai:derp", "triggered", "verbose")
@@ -231,18 +254,18 @@ def smart_action(Willie, trigger):
     Willie.debug("ai:derp", "owner: " + str(trigger.owner), "verbose")
     Willie.debug("ai:derp", "isop: " + str(trigger.isop), "verbose")
 basic_smart = "would you kindly|please|go"
-smart_action.rule = ("^$nickname[:,\s]+(%s)[A-Za-z0-9,'\s]+(NICKNAME)(a|an|the|some)(OBJECT)?")
+smart_action.rule = ("^$nickname[:,\s]+(%s)[A-Za-z0-9,'\s]+(NICKNAME)" +
+    "(a|an|the|some)(OBJECT)?")
 smart_action.priority = 'medium'
+"""
 
-nick_rule = r'^$nickname\s?!\s?$'
-nick_re = re.compile(nick_rule)
+
+@rule(ur'^$nickname\s?!\s?$')
 def nick(Willie, trigger):
     message = trigger.nick
     if re.match(Willie.nick.upper(), trigger.bytes):
         message = message.upper()
-    Willie.say(r'%s!' % message)
-nick.rule = nick_rule
-
+    Willie.say(ur'%s!' % message)
 
 
 if __name__ == "__main__":
