0753f77124ce12ed723cd4c3a5beb59cb1ff107f
Tim Dreyer <tdreyer1@gmail.com>
2013-07-12 20:12:56 -0500

    Updated for 4.0

--- bestpony.py
+++ bestpony.py
@@ -6,32 +6,32 @@ Licensed under the Eiffel Forum License 2.
 http://bitbucket.org/tdreyer/fineline
 """
 
-import time, random
+import time
+import random
+
+from willie.module import commands
 
 random.seed()
 
+
+@commands('bestpony')
 def bestpony(Willie, trigger):
     """Responds with who it thinks is the best pony."""
 
-    if random.uniform(0,3) > 2:
-        Willie.say(random.choice(("Well, let's see", "Hmm...", "Uh...",
-            "Oh, I know!", "That's easy!")))
-        time.sleep(random.uniform(1,3))
+    if random.uniform(0, 3) > 2:
+        Willie.say(random.choice([u"Well, let's see",
+                                  u"Hmm...",
+                                  u"Uh...",
+                                  u"Oh, I know!",
+                                  u"That's easy!"
+                                  ]))
+        time.sleep(random.uniform(1, 3))
 
-    Willie.say(random.choice((Willie.nick + '!', "I of course!", "I am!")))
+    Willie.say(random.choice((Willie.nick + '!', "I, of course!", "I am!")))
 
-    if random.uniform(0,20) > 19:
-        time.sleep(random.uniform(3,5))
+    if random.uniform(0, 20) > 19:
+        time.sleep(random.uniform(3, 5))
         Willie.say("Okay, just kidding. It's really Pinkie Pie.")
-#Match a command sequence eg !cmd
-bestpony.commands = ['bestpony']
-
-#Priorities of 'high', 'medium', and 'low' work
-bestpony.priority = 'low'
-
-#Limit in seconds of users ability to trigger module
-bestpony.rate = 60
-
 
 
 if __name__ == "__main__":
