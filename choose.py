a2bee59f4a7759fd0362f9b76b1ce64b0ad3498d
Tim Dreyer <tdreyer1@gmail.com>
2013-07-12 20:39:37 -0500

    Updated for 4.0

--- choose.py
+++ choose.py
@@ -8,17 +8,21 @@ http://bitbucket.org/tdreyer/fineline
 
 import random
 
+from willie.module import commands, example
+
 random.seed()
 
+
+@commands('choose')
+@example(ur"!choose 2, The Hobbit, Ender's Game, The Golden Compass")
 def choose(Willie, trigger):
-    """Returns a random selection of items provided to it.
+    """Returns a random selection of comma separated items provided to it.
     Chooses a subset if the first argument is an integer."""
 
     Willie.debug("choose.py", "==============", "verbose")
     Willie.debug("choose.py", "Module called.", "verbose")
-
     # Parse the provided arguments into a list of strings
-    Willie.debug("choose.py trigger.args",trigger.args, "verbose")
+    Willie.debug("choose.py trigger.args", trigger.args, "verbose")
     __, __, list = trigger.args[1].partition(' ')
     # Test for csv or space separated values
     if ',' in trigger.args[1]:
@@ -27,13 +31,10 @@ def choose(Willie, trigger):
         Willie.debug("choose.py args", args, "verbose")
     else:
         args = list.split()
-
     # Strip the strings
     for i, str in enumerate(args):
         args[i] = str.strip()
-
     Willie.debug("choose.py", args, "verbose")
-
     if len(args) > 1:
         # If the first argument is an int, we'll want to use it
         if args[0].isdigit():
@@ -50,14 +51,16 @@ def choose(Willie, trigger):
                 for i in range(choices):
                     last_choice = random.choice(args)
                     args.remove(last_choice)
-                    Willie.debug("choose.py", "Adding Choice " + last_choice,
-                            "verbose")
+                    Willie.debug("choose.py",
+                                 "Adding Choice " + last_choice,
+                                 "verbose")
                     choice_list.append(last_choice)
                 Willie.reply(', '.join(choice_list))
             else:
                 # The number is too small or too large to be useful
-                Willie.debug("choose.py", "Choice number is not sane.",
-                        "verbose")
+                Willie.debug("choose.py",
+                             "Choice number is not sane.",
+                             "verbose")
                 Willie.reply("Hmm, how about everything?")
         else:
             # Just choose one item since no number was specified
@@ -68,20 +71,6 @@ def choose(Willie, trigger):
         # <=1 items is not enough to choose from!
         Willie.debug("choose.py", "Not enough args.", "verbose")
         Willie.reply("You didn't give me enough to choose from!")
-# Match a command sequence eg !cmd
-choose.commands = ['choose']
-
-# Priorities of 'high', 'medium', and 'low' work
-choose.priority = 'medium'
-
-# Limit in seconds of users ability to trigger module
-choose.rate = 15
-
-# Example used in help query to bot for commands
-choose.example = '"!choose apple orange pear" or ' + \
-        '"!choose 2 red blue green orange" or ' + \
-        '"!choose 2, The Hobbit, Ender\'s Game, The Golden Compass"'
-
 
 
 if __name__ == "__main__":
