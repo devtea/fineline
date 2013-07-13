f3391b589c1ee6021d928c95e98f91d5b76abe48
Tim Dreyer <tdreyer1@gmail.com>
2013-07-12 22:26:56 -0500

    Updated for 4.0

--- colors.py
+++ colors.py
@@ -24,79 +24,85 @@ Recognized 'default' colors:
 from random import choice
 from types import *
 
-RESET = "\x0f"
+RESET = u"\x0f"
 COLORS = {
-        "white": "00",
-        "black": "01",
-        "dark blue": "02", "navy": "02",
-        "green": "03",
-        "red": "04",
-        "dark red": "05", "brown": "05", "maroon": "05",
-        "purple": "06", "violet": "06",
-        "orange": "07", "olive": "07",
-        "yellow": "08",
-        "light green": "09", "lime": "09",
-        "teal": "10", "blue cyan": "10",
-        "cyan": "11", "aqua": "11",
-        "blue": "12", "light blue": "12", "royal blue": "12",
-        "magenta": "13", "pink": "13", "light red": "13", "fuchsia": "13",
-        "dark grey": "14", "dark gray": "14", "grey": "14", "gray": "14",
-        "light grey": "15", "light gray": "15", "silver": "15"
-        }
+    u"white": u"00", u"0": u"00", u"00": u"00",
+    u"black": u"01", u"1": u"01", u"01": u"01",
+    u"dark blue": u"02", u"navy": u"02", u"2": u"02", u"02": u"02",
+    u"green": u"03", u"3": u"03", u"03": u"03",
+    u"red": u"04", u"4": u"04", u"04": u"04",
+    u"dark red": u"05", u"brown": u"05", u"maroon": u"05",
+    u"5": u"05", u"05": u"05",
+    u"purple": u"06", u"violet": u"06", u"6": u"06", u"06": u"06",
+    u"orange": u"07", u"olive": u"07", u"7": u"07", u"07": u"07",
+    u"yellow": u"08", u"8": u"08", u"08": u"08",
+    u"light green": u"09", u"lime": u"09", u"9": u"09", u"09": u"09",
+    u"teal": u"10", u"blue cyan": u"10", u"10": u"10",
+    u"cyan": u"11", u"aqua": u"11", u"11": u"11",
+    u"blue": u"12", u"light blue": u"12", u"royal blue": u"12", u"12": u"12",
+    u"magenta": u"13", u"pink": u"13", u"light red": u"13", u"fuchsia": u"13",
+    u"13": u"13",
+    u"dark grey": u"14", u"dark gray": u"14", u"grey": u"14", u"gray": u"14",
+    u"14": u"14",
+    u"light grey": u"15", u"light gray": u"15", u"silver": u"15", u"15": u"15"
+}
 STYLES = {
-        "i": "\x16", "italic": "\x16",
-        "u": "\x1F", "underline": "\x1F",
-        "b": "\x02", "bold": "\x02"
-        }
+    u"i": "\x16", u"italic": "\x16",
+    u"u": "\x1F", u"underline": "\x1F",
+    u"b": "\x02", u"bold": "\x02"
+}
+
 
 def colorize(text, colors=[], styles=[]):
-    assert isinstance(text, basestring), "No string provided."
-    assert text, "Text is empty."
-    assert type(colors) is ListType, "Colors must be in a list."
-    assert type(styles) is ListType, "Styles must be in a list."
-    assert len(colors) < 3, "Too many colors."
-    assert len(styles) < 4, "Too many styles."
-
+    assert isinstance(text, basestring), u"No string provided."
+    assert text, u"Text is empty."
+    assert type(colors) is ListType, u"Colors must be in a list."
+    assert type(styles) is ListType, u"Styles must be in a list."
+    assert len(colors) < 3, u"Too many colors."
+    assert len(styles) < 4, u"Too many styles."
     if colors or styles:
         message = u'%s' % text
         if len(colors) == 1:
             try:
-                message = u'\x03%s%s%s' % (COLORS[colors[0].lower()], message, RESET)
+                message = u'\x03%s%s%s' % (COLORS[colors[0].lower()],
+                                           message,
+                                           RESET
+                                           )
             except KeyError:
-                raise KeyError('Color "%s" is invalid.' % colors[0])
-        elif len(colors) ==2:
+                raise KeyError(u'Color "%s" is invalid.' % colors[0])
+        elif len(colors) == 2:
             try:
                 message = u'\x03%s,%s%s\x0f' % (
-                        COLORS[colors[0].lower()],
-                        COLORS[colors[1].lower()],
-                        message
-                        )
+                    COLORS[colors[0].lower()],
+                    COLORS[colors[1].lower()],
+                    message
+                )
             except KeyError:
-                raise KeyError('Color pair "%s, %s" is invalid.' % (
+                raise KeyError(u'Color pair "%s, %s" is invalid.' % (
                     colors[0],
                     colors[1]
-                    ))
+                ))
         if styles:
             for style in styles:
                 try:
                     message = u'%s%s\x0f' % (STYLES[style.lower()], message)
                 except KeyError:
-                    raise KeyError('Style "%s" is invalid.' % style)
+                    raise KeyError(u'Style "%s" is invalid.' % style)
         return message
     else:
         return text
 
 
 def rainbow(text):
-    assert isinstance(text, basestring), "No string provided."
-    assert text, "Text is empty."
-    rainbow = ['black', 'red', 'navy', 'green', 'purple', 'pink']
+    assert isinstance(text, basestring), u"No string provided."
+    assert text, u"Text is empty."
+    rainbow = [u'black', u'red', u'navy', u'green', u'purple', u'pink']
     message = u''
     for c in text:
         message = u'%s%s' % (
-                message,
-                colorize(c, [rainbow[choice(range(len(rainbow)))]])
-                )
+            message,
+            colorize(c, [rainbow[choice(range(len(rainbow)))]])
+        )
     message = u'%s%s' % (message, RESET)
     return message
 
