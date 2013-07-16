b5815fdef7d30c1ea49be0605445b33126966b17
Tim Dreyer <tdreyer1@gmail.com>
2013-07-16 12:47:36 -0500

    Updated for 4.0

--- uptime.py
+++ uptime.py
@@ -8,39 +8,39 @@ http://bitbucket.org/tdreyer/fineline
 import time
 from datetime import timedelta
 
+from willie.module import commands
+
+
 def setup(Willie):
     if "uptime" not in Willie.memory:
-        Willie.debug(
-                "uptime:startup",
-                "Found no time, adding.",
-                "verbose"
-                )
+        Willie.debug(u"uptime:startup",
+                     u"Found no time, adding.",
+                     u"verbose"
+                     )
         Willie.memory["uptime"] = int(time.time())
     else:
-        Willie.debug(
-                "uptime:startup",
-                "Found time.",
-                "verbose"
-                )
+        Willie.debug(u"uptime:startup",
+                     u"Found time.",
+                     u"verbose"
+                     )
 
 
+@commands('uptime')
 def uptime(Willie, trigger):
     now = int(time.time())
     then = Willie.memory["uptime"]
-    diff = str(timedelta(seconds=now-then))
-    Willie.debug("uptime", diff, "verbose")
-    Willie.debug("uptime", len(diff), "verbose")
+    diff = str(timedelta(seconds=now - then))
+    Willie.debug(u"uptime", diff, u"verbose")
+    Willie.debug(u"uptime", len(diff), u"verbose")
     if len(diff) < 9:
         h, m, s = diff.split(":")
         d = '0 days'
     else:
         d, m, s = diff.split(":")
         d, h = d.split(", ")
-    Willie.say(("I have had %s, %s hours, %s minutes and %s " +
-        "seconds of uptime.") % (d, h, m, s))
-uptime.commands = ['uptime']
-uptime.priority = 'low'
-uptime.rate = 300
+    Willie.say((u"I have had %s, %s hours, %s minutes and %s " +
+                u"seconds of uptime.") % (d, h, m, s)
+               )
 
 
 if __name__ == "__main__":
