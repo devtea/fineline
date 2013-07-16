77e0e6d909e2145cb8c648bebb1962f475e6eea9
Tim Dreyer <tdreyer1@gmail.com>
2013-07-15 20:36:49 -0500

    Updated for 4.0

--- ping.py
+++ ping.py
@@ -5,23 +5,13 @@ Licensed under the Eiffel Forum License 2.
 
 http://bitbucket.org/tdreyer/fineline
 """
+from willie.module import commands, priority
 
+
+@commands(u'ping')
+@priority(u'high')
 def ping(Willie, trigger):
-    """Responds to a user's ping"""
-
-    Willie.say(r'Pony!')
-# Match a command sequence eg !cmd
-ping.commands = ['ping']
-
-# Priorities of 'high', 'medium', and 'low' work
-ping.priority = 'high'
-
-# Willie is multithreaded by default.
-#template.thread = False
-
-# Limit in seconds of users ability to trigger module
-ping.rate = 60
-
+    Willie.say(u'Pony!')
 
 
 if __name__ == "__main__":
