bcf61ba6118a6b7c5a330bea199523a39779d079
Tim Dreyer <tdreyer1@gmail.com>
2013-07-15 22:20:32 -0500

    Updated for 4.0

--- prompt.py
+++ prompt.py
@@ -5,44 +5,43 @@ Licensed under the Eiffel Forum License 2.
 
 http://bitbucket.org/tdreyer/fineline
 """
-import random, bisect
+
+import random
+import bisect
+
+from willie.module import commands
 
 random.seed()
 
-def setup(Willie):
-    #Load from database when module is first loaded to reduce load over
-    #each call
 
+def setup(Willie):
     #Load list of names
     global ponies
     ponies = []
     for row in Willie.db.prompt_ponies.keys():
-        """Willie.debug("prompt.py", Willie.db.prompt_ponies.get(row[0],
-            ('name','weight')), "verbose")"""
-        ponies.append(Willie.db.prompt_ponies.get(row[0], ('name','weight')))
-    Willie.debug("prompt.py", "Loaded " + str(len(ponies)) +
-            " weighted ponies.", "verbose")
-
+        ponies.append(Willie.db.prompt_ponies.get(row[0], ('name', 'weight')))
+    Willie.debug(u"prompt.py",
+                 u"Loaded %s weighted ponies." % str(len(ponies)),
+                 u"verbose"
+                 )
     #Load list of nouns
     global nouns
     nouns = []
     for row in Willie.db.prompt_nouns.keys():
-        """Willie.debug("prompt.py", Willie.db.prompt_ponies.get(row[0],
-            ('name','weight')), "verbose")"""
         nouns.append(Willie.db.prompt_nouns.get(row[0], 'noun'))
-    Willie.debug("prompt.py", "Loaded " + str(len(nouns)) +
-            " nouns.", "verbose")
-
+    Willie.debug(u"prompt.py",
+                 u"Loaded %s nouns." % str(len(nouns)),
+                 u"verbose"
+                 )
     #Load list of verbs
     global verbs
     verbs = []
     for row in Willie.db.prompt_verbs.keys():
-        """Willie.debug("prompt.py", Willie.db.prompt_ponies.get(row[0],
-            ('name','weight')), "verbose")"""
         verbs.append(Willie.db.prompt_verbs.get(row[0], 'verb'))
-    Willie.debug("prompt.py", "Loaded " + str(len(verbs)) +
-            " verbs.", "verbose")
-
+    Willie.debug(u"prompt.py",
+                 u"Loaded %s verbs." % str(len(verbs)),
+                 u"verbose"
+                 )
 
 
 def weighted_choice(weighted):
@@ -55,36 +54,23 @@ def weighted_choice(weighted):
     for item in weighted:
         sum = sum + int(item[1])
         sum_steps.append(sum)
-    return bisect.bisect_right( sum_steps, random.uniform(0,sum))
+    return bisect.bisect_right(sum_steps, random.uniform(0, sum))
 
 
-
+@commands(u'prompt')
 def prompt(Willie, trigger):
     """Gives a short drawing prompt using ponies from the show."""
 
-    Willie.debug("prompt.py", "==============", "verbose")
-    Willie.debug("prompt.py", "Module started", "verbose")
-
+    Willie.debug(u"prompt.py", u"==============", u"verbose")
+    Willie.debug(u"prompt.py", u"Module started", u"verbose")
     #Make our random selections for our prompt construction
     index_no = weighted_choice(ponies)
-    sentence = ["Your random prompt is: ",
-            ponies[index_no][0],
-            random.choice(verbs).strip(),
-            random.choice(nouns).strip() + "."]
-
-    Willie.reply(" ".join(sentence))
-#Match a command sequence eg !cmd
-prompt.commands = ['prompt']
-
-#Priorities of 'high', 'medium', and 'low' work
-prompt.priority = 'medium'
-
-#Willie is multithreaded by default.
-#prompt.thread = False
-
-#Limit in seconds of users ability to trigger module
-prompt.rate = 35
-
+    sentence = [u"Your random prompt is: ",
+                ponies[index_no][0],
+                random.choice(verbs).strip(),
+                random.choice(nouns).strip() + u"."
+                ]
+    Willie.reply(u" ".join(sentence))
 
 
 if __name__ == "__main__":
