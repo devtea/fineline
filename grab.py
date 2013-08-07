26062dcc05209a1db0b284bbdf2bc29df8b907bb
Tim Dreyer <tdreyer1@gmail.com>
2013-08-06 19:19:49 -0500

    Fixed issue with the order in which it kept things.

--- grab.py
+++ grab.py
@@ -6,13 +6,12 @@ Licensed under the Eiffel Forum License 2.
 http://bitbucket.org/tdreyer/fineline
 """
 from willie.module import commands, rule, example
-from collections import deque
 from willie.tools import Nick
 import threading
 
 
 def setup(bot):
-    bot.memory['grab'] = deque(maxlen=1000)
+    bot.memory['grab'] = {}
     bot.memory['grablock'] = threading.Lock()
 
 
@@ -27,11 +26,11 @@ def grab(bot, trigger):
     else:
         bot.memory['grablock'].acquire()
         try:
-            for b, n in bot.memory['grab']:
-                if n == target:
-                    bot.say(u'!addquote <%s> %s' % (n, b))
-                    return
-            bot.say(u'Sorry, nothing to grab!')
+            if target in bot.memory['grab']:
+                bot.say(u'!addquote <%s> %s' % (target,
+                                                bot.memory['grab'][target]))
+            else:
+                bot.say(u'Sorry, nothing to grab!')
         finally:
             bot.memory['grablock'].release()
 
@@ -40,8 +39,7 @@ def grab(bot, trigger):
 def recent_watcher(bot, trigger):
     bot.memory['grablock'].acquire()
     try:
-        print "recording thing"
-        bot.memory['grab'].append({Nick(trigger.nick), trigger.bytes})
+        bot.memory['grab'][Nick(trigger.nick)] = trigger.bytes
     finally:
         bot.memory['grablock'].release()
 
