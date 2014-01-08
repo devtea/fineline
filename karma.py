2b9192e5e3af3463ec802fe359c2a1b6bf8f8e7d
Tim Dreyer <tdreyer1@gmail.com>
2014-01-08 09:07:26 -0600

    Integrated database storage, minor formatting and bugfixing.

--- karma.py
+++ karma.py
@@ -1,6 +1,6 @@
 """
 karma.py - A willie module to keep track of "points" for arbitrary things
-Copyright 2013, Tim Dreyer
+Copyright 2013, Khyperia, Tim Dreyer
 Licensed under the Eiffel Forum License 2.
 
 http://bitbucket.org/tdreyer/fineline
@@ -11,59 +11,100 @@ import time
 
 from willie.module import commands, example, rule, priority
 
-def setup(willie):
-    if 'karma_lock' not in willie.memory:
-        willie.memory['karma_lock'] = threading.Lock()
-    if 'karma' not in willie.memory:
-        willie.memory['karma'] = {}
-    if 'karma_time' not in willie.memory:
-        willie.memory['karma_time'] = {}
+
+def setup(bot):
+    if 'karma_lock' not in bot.memory:
+        bot.memory['karma_lock'] = threading.Lock()
+    if 'karma_time' not in bot.memory:
+        bot.memory['karma_time'] = {}
+    if 'karma' in bot.memory:
+        for i in bot.memory['karma']:
+            print '%s: %i' % (i, bot.memory['karma'][i])
+    print 'klearing karma'
+    bot.memory['karma'] = {}
+    if 'karma' in bot.memory:
+        for i in bot.memory['karma']:
+            print '%s: %i' % (i, bot.memory['karma'][i])
+
+    with bot.memory['karma_lock']:
+        dbcon = bot.db.connect()  # sqlite3 connection
+        cur = dbcon.cursor()
+        try:
+            #if our tables don't exist, create them
+            cur.execute('''CREATE TABLE IF NOT EXISTS karma
+                           (tag text, score int)''')
+            dbcon.commit()
+
+            cur.execute('SELECT tag, score from karma')
+            dbload = cur.fetchall()
+        finally:
+            cur.close()
+            dbcon.close()
+        if dbload:
+            for t, s in dbload:
+                bot.memory['karma'][t] = s
+
 
 @priority(u'low')
 @rule(u'.*')
-def karmaRule(willie, trigger):
+def karmaRule(bot, trigger):
     if trigger.sender[0] != '#':
         return
-    obj = trigger.args[1].split()
-    if not obj:
+    obj = trigger.bytes.strip()
+    if not obj or len(obj) < 3:
         return
-    obj = obj[0]
-    if len(obj) < 3:
-        return
-    shortobj = obj[:-2]
-    
-    if obj.endswith("++") and timecheck(willie, trigger):
-        modkarma(willie, shortobj, 1)
-    elif obj.endswith("--") and timecheck(willie, trigger):
-        modkarma(willie, shortobj, 1)
+    shortobj = obj[:-2].lower().strip()
 
-def timecheck(willie, trigger):
-    willie.memory['karma_lock'].acquire()
-    try:
-        if trigger.sender in willie.memory['karma_time'] and time.time() < willie.memory['karma_time'][trigger.sender] + 60:
-	    willie.reply(u"You just used karma! You can't use it again for a bit.")
-	    return False
-	willie.memory['karma_time'][trigger.sender] = time.time()
+    with bot.memory['karma_lock']:
+        newkarm = None
+        if obj.endswith("++") and timecheck(bot, trigger):
+            newkarm = modkarma(bot, shortobj, 1)
+        elif obj.endswith("--") and timecheck(bot, trigger):
+            newkarm = modkarma(bot, shortobj, -1)
+
+    if newkarm:
+        bot.reply("Karma for %s is at %i" % (shortobj, newkarm))
+
+
+def timecheck(bot, trigger):
+    if trigger.admin:
         return True
-    finally:
-        willie.memory['karma_lock'].release()
+    if trigger.sender in bot.memory['karma_time'] and time.time() < bot.memory['karma_time'][trigger.sender] + 60:
+        bot.reply(u"You just used karma! You can't use it again for a bit.")
+        return False
+    bot.memory['karma_time'][trigger.sender] = time.time()
+    return True
+
 
 @commands('karma')
 @example(u'!karma fzoo')
-def karma(willie, trigger):
-    modkarma(willie, trigger.args[1].split()[1], 0)
+def karma(bot, trigger):
+    obj = trigger.bytes[7:].lower().strip()
+    karm = modkarma(bot, obj, 0)
+    if karm:
+        bot.reply("Karma for %s is at %i" % (obj, karm))
 
-def modkarma(willie, obj, amount):
-    willie.memory['karma_lock'].acquire()
+
+def modkarma(bot, obj, amount):
+    dbcon = bot.db.connect()  # sqlite3 connection
+    cur = dbcon.cursor()
+
     try:
-        if obj in willie.memory['karma']:
-	    willie.memory['karma'][obj] += amount
-	else:
-	    willie.memory['karma'][obj] = amount
-	
-	willie.reply("Karma for %s is at %i" % (obj, willie.memory['karma'][obj]))
+        if obj in bot.memory['karma']:
+            bot.memory['karma'][obj] += amount
+            cur.execute('''UPDATE karma set score = ?
+                        where tag = ?''',
+                        (bot.memory['karma'][obj], obj))
+        else:
+            bot.memory['karma'][obj] = amount
+            cur.execute('''INSERT into karma (tag, score)
+                            VALUES (?, ?)''',
+                        (obj, bot.memory['karma'][obj]))
+        dbcon.commit()
+        return bot.memory['karma'][obj]
     finally:
-        willie.memory['karma_lock'].release()
+        cur.close()
+        dbcon.close()
 
 
 if __name__ == "__main__":
