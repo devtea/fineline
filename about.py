41bd72447078398555800f836495230a5c890ad8
Tim Dreyer <tdreyer1@gmail.com>
2014-01-09 15:25:34 -0600

    Updated info.

--- about.py
+++ about.py
@@ -8,48 +8,48 @@ http://bitbucket.org/tdreyer/fineline
 
 import random
 import time
-
 from willie.module import commands
 
 random.seed()
 
 
 @commands('about')
-def about(Willie, trigger):
+def about(bot, trigger):
     """Shares basic information on the bot."""
+    owner = bot.config.core.owner
+
     time.sleep(random.uniform(0, 3))
-    Willie.say(u"Hello, my name is %s and I'm a pony! " % Willie.nick +
-               u"http://mlpdrawingschool.deviantart.com/gallery/37232754")
+    bot.say(u"Hello, my name is %s and I'm a pony! " % bot.nick +
+            u"http://mlpdrawingschool.deviantart.com/gallery/37232754")
     time.sleep(random.uniform(3, 5))
-    Willie.say(u"Well, that's not exactly right. I'm speaking to you " +
-               u"through an implementation of the Willie bot hosted by " +
-               u"tdreyer1.")
+    bot.say(u"Well, that's not exactly right. I'm speaking to you " +
+            u"through an implementation of the bot bot hosted by " +
+            u"%s." % owner)
     time.sleep(random.uniform(3, 5))
-    Willie.say(u"I'm also open source! You can see my source at " +
-               u"http://willie.dftba.net/ and my plugins at " +
-               u"http://bitbucket.org/tdreyer/fineline")
+    bot.say(u"I'm also open source! You can see my source at " +
+            u"http://willie.dftba.net/ and my plugins at " +
+            u"http://bitbucket.org/tdreyer/fineline")
 
 
 @commands('bugs', 'bug')
-def bugs(Willie, trigger):
+def bugs(bot, trigger):
     """Shares basic bug reporting information for the bot."""
     time.sleep(random.uniform(0, 3))
-    Willie.say(u'[](/derpyshock "Bugs?! I don\'t have any bugs!")')
+    bot.say(u'[](/derpyshock "Bugs?! I don\'t have any bugs!")')
     time.sleep(random.uniform(4, 6))
-    Willie.say(u"But I guess if you think you've found one, you can either " +
-               u"message my owner, tdreyer1, or you can make a bug report " +
-               u"at https://bitbucket.org/tdreyer/fineline/issues")
+    bot.say(u"But I guess if you think you've found one, you can " +
+            u"make a bug report at https://bitbucket.org/tdreyer/fineline/issues")
 
 
 @commands('source')
-def source(Willie, trigger):
+def source(bot, trigger):
     """Gives links to the bot's source code"""
     time.sleep(random.uniform(0, 3))
-    Willie.say('[](/ppnervous "My what?")')
+    bot.say('[](/ppnervous "My what?")')
     time.sleep(random.uniform(3, 5))
-    Willie.say(u"Well I guess it's okay, since it's you. You can see my " +
-               u"source at http://willie.dftba.net/ and my plugins at " +
-               u"http://bitbucket.org/tdreyer/fineline")
+    bot.say(u"Well I guess it's okay, since it's you. You can see my " +
+            u"source at http://willie.dftba.net/ and my plugins at " +
+            u"http://bitbucket.org/tdreyer/fineline")
 
 
 if __name__ == "__main__":
