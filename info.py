5f3e41e90d8f251d86eda536c53fd2db0e1a54b2
Tim Dreyer <tdreyer1@gmail.com>
2014-01-09 19:49:56 -0600

    Re-enabled help command and added channel exclusion.

--- info.py
+++ info.py
@@ -8,35 +8,39 @@ http://bitbucket.org/tdreyer/fineline
 
 from willie.module import commands, rule, example, priority
 
+_EXCLUDE = ['#reddit-mlpds']
+
 
 @rule(u'$nick' '(?i)(help|doc) +([A-Za-z]+)(?:\?+)?$')
 @example(u'!help seen')
 @commands(u'help')
 @priority(u'low')
-def doc(willie, trigger):
+def doc(bot, trigger):
     """Shows a command's documentation, and possibly an example."""
-    name = trigger.group(2)
-    if name:
-        name = name.lower()
+    if not trigger.group(2):
+        if trigger.sender in _EXCLUDE:
+            return
+        bot.reply('Say !help <command> (for example !help seen) to get help for a command, or !commands for a list of commands.')
     else:
-        return
+        name = trigger.group(2)
+        name = name.lower()
 
-    if (name in willie.doc
-            and not willie.doc[name][0].startswith(u"ADMIN")):
-        willie.reply(willie.doc[name][0])
-        if willie.doc[name][1]:
-            willie.say(u'e.g. ' + willie.doc[name][1])
+        if (name in bot.doc
+                and not bot.doc[name][0].startswith(u"ADMIN")):
+            bot.reply(bot.doc[name][0])
+            if bot.doc[name][1]:
+                bot.say(u'e.g. ' + bot.doc[name][1])
 
 
 @commands(u'commands')
 @priority(u'low')
-def commands(willie, trigger):
-    """Return a list of Willie's commands"""
+def commands(bot, trigger):
+    """Return a list of the bot's commands"""
     if trigger.owner:
-        names = u', '.join(sorted(willie.doc.iterkeys()))
+        names = u', '.join(sorted(bot.doc.iterkeys()))
     else:
-        cmds = [i for i in sorted(willie.doc.iterkeys())
-                if not willie.doc[i][0].startswith(u"ADMIN")
+        cmds = [i for i in sorted(bot.doc.iterkeys())
+                if not bot.doc[i][0].startswith(u"ADMIN")
                 and i not in [u'newoplist',
                               u'listops',
                               u'listvoices',
@@ -46,19 +50,19 @@ def commands(willie, trigger):
                               ]  # bad hack for filtering admin cmds
                 ]
         names = u', '.join(sorted(cmds))
-    willie.reply(u'Commands I recognise: ' + names + u'.')
-    willie.reply(u"For help, do '!help example' where example is the " +
-                 u"name of the command you want help for.")
+    bot.reply(u'Commands I recognise: ' + names + u'.')
+    bot.reply(u"For help, do '!help example' where example is the " +
+              u"name of the command you want help for.")
 
 
 @rule('$nick' r'(?i)help(?:[?!]+)?$')
 @priority('low')
-def help(willie, trigger):
+def help(bot, trigger):
     response = (
         u'Hi! I\'m %s and I\'m a pony. Say "!commands" to me in private ' +
         u'for a list of the things I can do. Say hi to my master, %s!'
-    ) % (willie.nick, willie.config.owner)
-    willie.reply(response)
+    ) % (bot.nick, bot.config.owner)
+    bot.reply(response)
 
 
 if __name__ == '__main__':
