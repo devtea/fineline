ff508118c38930aee493e109fe7890662b4a351e
Tim Dreyer <tdreyer1@gmail.com>
2014-01-11 08:27:15 -0600

    Merging in change from upstream. fix accessing exclude list in setup() https://github.com/embolalia/willie/commit/3591190514c851fcd46c0af8f8583f872dcf8c48

--- url_.py
+++ url_.py
@@ -59,7 +59,7 @@ def setup(bot=None):
 
     if bot.config.has_option('url', 'exclude'):
         regexes = [re.compile(s) for s in
-                   bot.config.url.get_list(bot.config.url.exclude)]
+                   bot.config.url.get_list('exclude')]
     else:
         regexes = []
 
