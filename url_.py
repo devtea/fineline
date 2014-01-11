d7a34e4e5bc2e52c825e883b928ea9aab3019c8a
Tim Dreyer <tdreyer1@gmail.com>
2014-01-11 12:23:55 -0600

    Removed unnecessary debug line.

--- url_.py
+++ url_.py
@@ -203,7 +203,6 @@ def check_callbacks(bot, trigger, url, run=True):
     """
     # Check if it matches the exclusion list first
     if bot.memory['url_exclude']:
-        print bot.memory['url_exclude']
         matched = any(regex.search(url) for regex in bot.memory['url_exclude'])
     # Then, check if there's anything in the callback list
     for regex, function in bot.memory['url_callbacks'].iteritems():
