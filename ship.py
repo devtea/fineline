a1d046231cbb4150d84ae551d12e16ae3d22b22d
Tim Dreyer <tdreyer1@gmail.com>
2014-01-13 19:26:50 -0600

    Added command to insert new characters into database.

--- ship.py
+++ ship.py
@@ -64,14 +64,44 @@ def weighted_choice(weighted):
 @commands(u'ship')
 def ship(bot, trigger):
     """Returns a somewhat random shipping pair."""
-    index_no = weighted_choice(bot.memory['pony_list'])
-    i2 = index_no
-    while i2 == index_no:
+    i1 = weighted_choice(bot.memory['pony_list'])
+    i2 = i1
+    while i2 == i1:
         i2 = weighted_choice(bot.memory['pony_list'])
-    pair = (bot.memory['pony_list'][index_no][0],
+    pair = (bot.memory['pony_list'][i1][0],
             bot.memory['pony_list'][i2][0]
             )
     bot.reply(random.choice(_reply_list) % pair)
+
+
+@commands(u'addpony')
+def addpony(bot, trigger):
+    '''ADMIN: Adds a pony to the database. Admin only.'''
+    if not trigger.admin:
+        return
+    command = trigger.args[1].split(' ')
+    if len(command) < 3:
+        bot.reply('Not enough arguments. Needs name and weight.')
+        return
+    try:
+        weight = int(command.pop(-1))
+    except:
+        bot.reply('Invalid weight. Final argument must be an integer.')
+        return
+    command.pop(0)
+
+    name = ' '.join(command)
+    print 'name: "%s", weight: "%i"' % (name, weight)
+    with bot.memory['pony_list_lock']:
+        dbcon = bot.db.connect()
+        cur = dbcon.cursor()
+        try:
+            cur.execute('''insert into prompt_ponies (name, weight)
+                           values (?, ?)''', (name, weight))
+            cur.commit()
+        finally:
+            cur.close()
+            dbcon.close()
 
 
 if __name__ == "__main__":
