c5fb3d75b4a369885cfa79ae3bff61162e45364d
Tim Dreyer <tdreyer1@gmail.com>
2014-07-27 10:47:06 -0500

    PEP8ification

--- poon.py
+++ poon.py
@@ -1,19 +1,16 @@
 # coding=utf-8
 """
 poon.py - A simple Willie module to return links to derpy faces
-Copyright 2013, Tim Dreyer
+Copyright 2013, Tim Dreyer, Porter Smith
 Licensed under the Eiffel Forum License 2.
 
 http://bitbucket.org/tdreyer/fineline
-
-Requires the following in the config:
-[imgur]
-client_id = ______
-client_secret = _____
 """
 from __future__ import print_function
 
-import json, urllib2, random
+import json
+import urllib2
+import random
 
 from willie.module import commands
 
@@ -33,39 +30,48 @@ except:
         if fp:
             fp.close()
 
+_imgur_api_url = 'https://api.imgur.com/3/'
+_poon_album = 'YZmbK'
+
+
 def setup(bot):
+    """
+    | [imgur] | example | purpose |
+    | client_id | k1j3k1lk388 | client_id from a registered imgur appliction |
+    | client_secret | skj1831k3j1j31lk31k31j3lk3424514k | client_secret from a registered imgur application |
+    """
     bot.memory["imgur_client"] = bot.config.imgur.client_id
     update_poon(bot)
 
-imgur_api_url = 'https://api.imgur.com/3/'
-poon_album = 'YZmbK'
 
 def imgur_anonymous_request(client_id, endpoint):
-    req = urllib2.Request(imgur_api_url + endpoint)
+    req = urllib2.Request(_imgur_api_url + endpoint)
     req.add_header('authorization', 'client-id ' + client_id)
     res = urllib2.urlopen(req)
     data = json.loads(res.read().decode('utf-8'))
     return data
 
+
 def imgur_album_data(client_id, album_id):
     endpoint = 'album/' + album_id
     return imgur_anonymous_request(client_id, endpoint)
 
+
 def update_poon(bot):
     bot.memory['poon_images'] = []
-    data = imgur_album_data(bot.memory['imgur_client'], poon_album)
+    data = imgur_album_data(bot.memory['imgur_client'], _poon_album)
     for image in data['data']['images']:
         bot.memory['poon_images'].append(unicode(image['link']))
+
 
 @commands(u'poon')
 def poon(bot, trigger):
     # Don't do anything if the bot has been shushed
     if bot.memory['shush']:
         return
-    # if we don't have any images then do nothing
+    # If we don't have any images then do nothing
     if not bot.memory['poon_images'] or len(bot.memory['poon_images']) == 0:
         return
-
     url = random.choice(bot.memory['poon_images'])
     bot.say(url)
 
