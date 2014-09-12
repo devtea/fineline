0e6154e959084589161f8cb40c79e07f13c8038e
Tim Dreyer <tdreyer1@gmail.com>
2014-09-11 20:38:03 -0500

    updates

--- choose.py
+++ choose.py
@@ -82,7 +82,7 @@ def weighted_choice(bot, unweighted):
 
     good = ['art', 'arting', 'artistic', 'arty', 'create', 'creative', 'draw',
             'drawing', 'paint', 'painting', 'sketch', 'sketching', 'study',
-            'studies', 'studying', 'aeyrt', 'tar', 'tea', 'cook']
+            'studies', 'studying', 'aeyrt', 'tar', 'tea', 'cook', 'productive']
     bad = ['don\'t', 'dont', 'fuck', 'later', 'no', 'not', 'never', 'quit',
            'lame', 'stupid', 'dumb', 'bad', 'out', 'sucks', 'sucky', 'worse',
            'hitler', 'fcuk', 'fook', 'fock', 'stop', 'but', 'dum']
